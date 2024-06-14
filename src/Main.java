24fbd47b008ecdf084bc52673e02fd51a7e38519
joaoVtr <joao_vitor_mj@hotmail.com>
2024-06-14 07:55:25 -0300

    Lista linkada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import dataStructure.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
---
+++ src/dataStructure/LinkedList.java
@@ -0,0 +1,108 @@
+package dataStructure;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+        }
+    }
+
+    public LinkedList(String data){
+        length = 1 ;
+        Node newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public void getHead(){
+        if(this.head == null){
+            System.out.println("Lista Vazia");
+        }else{
+            System.out.println("Head: "+ head.data);
+        }
+    }
+
+    public void getTail(){
+        if(this.tail == null){
+            System.out.println("Lista Vazia");
+        }else{
+            System.out.println("Tail: "+ tail.data);
+        }
+    }
+
+    public void getLength(){
+        System.out.println("Length: "+this.length);
+    }
+
+    public void makeEmpty(){
+        head = null;
+        tail = null;
+    }
+
+    public void print(){
+        System.out.println("################################");
+        Node temp = this.head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+        System.out.println("################################");
+    }
+
+    public void append(String data){
+        Node newNode = new Node(data);
+        if(length ==0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length ++;
+    }
+
+    public Node removeLast(){
+        if(length == 0 ) return null;
+        Node pre = head;
+        Node temp = null;
+        while(pre.next != tail){
+            pre = pre.next;
+        }
+
+        temp = tail;
+        tail = pre;
+        tail.next = null;
+
+        length --;
+        if(length == 0 ){
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList("Elemento 1 ");
+        list.append("Elemento 2 ");
+        list.append("Elemento 3 ");
+
+        list.getHead();
+        list.getTail();
+        list.getLength();
+        list.print();
+
+        System.out.println("Removendo ultimo elemento");
+        System.out.println(list.removeLast().data);
+        list.print();
+    }
+
+}
