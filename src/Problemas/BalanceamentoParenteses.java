3917283b41ed32d45ff503564507f48c16b05fcf
joaoVtr <joao_vitor_mj@hotmail.com>
2024-06-16 10:46:32 -0300

    Balanceamento de parenteses

---
+++ src/Problemas/BalanceamentoParenteses.java
@@ -0,0 +1,31 @@
+package Problemas;
+
+import java.util.Set;
+import java.util.Stack;
+
+public class BalanceamentoParenteses {
+
+    public  static boolean solve(String expression){
+        Set<Character> openSymbols = Set.of('(', '{', '[');
+        Set<Character> closeSymbols = Set.of(')', '}', ']');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: expression.toCharArray()){
+            if(openSymbols.contains(c)) stack.push(c);
+            if(closeSymbols.contains(c)){
+                if(stack.isEmpty()) return false;
+                var open = stack.pop();
+                if(open == '(' && c != ')' ) return false;
+                if(open == '[' && c != ']' ) return false;
+                if(open == '{' && c != '}' ) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve("2*(3*4+5*[2+3)]"));
+        System.out.println(solve("2*(3*4+5*[2+3])"));
+    }
+}
